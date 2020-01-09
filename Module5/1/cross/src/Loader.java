a433158ae186226f2e05237def28482b6acfd289
Anastasiya <59449438+StuChuProg@users.noreply.github.com>
2020-01-09 14:33:56 +0300

    Update Loader.java

--- Module5/1/cross/src/Loader.java
+++ Module5/1/cross/src/Loader.java
@@ -1,16 +1,17 @@
 public class Loader {
     public static void main(String[] args) {
-        String[][] cross = {
-                {"X", " ", " ", " ", "X"},
-                {" ", "X", " ", "X", " "},
-                {" ", " ", "X", " ", " "},
-                {" ", "X", " ", "X", " "},
-                {"X", " ", " ", " ", "X"},
-        };
+        String[][] cross = new String[5][5];
 
-        for (int i =0; i < cross.length; i++) {
-            for (int j = 0; j < cross[i].length; j++) {
-                System.out.print(cross[i][j]);
+        for (int numRows=0; numRows < cross.length; numRows++){
+            for(int numCols = 0; numCols < cross[numRows].length; numCols++){
+                if ((numCols+numRows == 4) || (numCols == numRows)){
+                    cross[numRows][numCols] = "X";
+                }else cross[numRows][numCols] = " ";
+            }
+        }
+        for (int i = 0; i < cross.length; i++) {
+            for (int j = 0; j < cross[0].length; j++) {
+                System.out.print(" " + cross[i][j] + " ");
             }
             System.out.println();
         }
