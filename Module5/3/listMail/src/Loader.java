5c5d9f8c05dcfd4a48e938000af7b603251d913b
ana <anas-chuamkova@yandex.ru>
2020-01-15 23:32:50 +0300

    add code, who work and reports the incorrect input

--- Module5/3/listMail/src/Loader.java
+++ Module5/3/listMail/src/Loader.java
@@ -2,8 +2,8 @@ import java.util.*;
 
 public class Loader {
     public static void main(String[] args){
-        System.out.println("Введите команду");
-
+        System.out.println("Введите e-Mail начиная с команды ADD");
+        TreeSet<String> mail = new TreeSet<>();
         Scanner scanner = new Scanner(System.in);
         while (true) {
             System.out.println("'EXIT'  для выхода");
@@ -11,16 +11,10 @@ public class Loader {
             if (command.trim().equals("EXIT")) {
                 break;
             }
-
-            TreeSet<String> mail = new TreeSet<>();
-
-            if (command.matches("ADD\\s\\D+")) {
+            if (command.matches("ADD\\s\\b[A-Za-z0-9._%+-]+@[A-Za-z0-9]+\\.[A-Za-z]{2,4}\\b")) {
                 String[] comm = command.split("\\s+", 2);
                 mail.add(comm[1]);
-            }
-            for (String eMail : mail) {
-                System.out.println(eMail);
-            }
+            }else {System.out.println("пожалуйста, проверьте правильность написаниия e-Mail");}
             if (command.matches("LIST")) {
                 for (String eMail : mail) {
                     System.out.println(eMail);
