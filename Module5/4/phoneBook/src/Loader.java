f6fa47bc8c81235d6cf69b640006d45ff844413f
ana <anas-chuamkova@yandex.ru>
2020-01-19 10:21:17 +0300

    add code phoneBook change

--- Module5/4/phoneBook/src/Loader.java
+++ Module5/4/phoneBook/src/Loader.java
@@ -4,8 +4,9 @@ public class Loader {
     public static void main(String[] args){
         System.out.println("Пожалуйста, введите телефон и/или имя");
         TreeMap<String, String> phoneBook = new TreeMap<>();
-
         Scanner scanner = new Scanner(System.in);
+        String name = " ";
+       String number = " ";
         for (;;){
 
             String sc = scanner.nextLine().trim();
@@ -13,41 +14,51 @@ public class Loader {
                 printMap(phoneBook);
                 continue;
             }
-            String name;
-            String number;
-            if (sc.matches("\\d")){
-                System.out.println("Введите имя абонента");
+
+          //  int key = 0;
+        //    int count = 0;
+           // for (int i = 0; i < phoneBook.size(); i++) {
+             //   if (phoneBook.containsKey(name)) {
+               //     count++;
+            //        key = i;
+              //      continue;}
+          //      if (phoneBook.containsValue(number)) {
+            //        count++;
+              //      key = i;;
+                //    continue; }}
+        //    if (count >= 2){
+        //        System.out.println(phoneBook.get(name) + " < = > " + phoneBook.get(phoneBook.get(key))); }
+
+            if (sc.matches("\\d+")){
                 name = sc;
+                System.out.println("Введите имя абонента");
+
             }
-            if (sc.matches("\\^[A-Za-zА-яёЁ]")) {
+            if (sc.matches("\\b[A-Za-zА-яёЁ]+")) {
                 System.out.println("Введите телефон");
                 number = sc;
             }
-            if (sc.matches("\\^[A-Za-zА-яёЁ]\\s+\\d")){
+            if (sc.matches("\\b[A-Za-zА-яёЁ]+\\s+\\d+")){
+
                 String[] com = sc.split("\\s+", 2);
                 name = com[0];
                 number = com[1];
             }
-            if (sc.matches("\\d\\s+\\b[A-Za-zА-яёЁ]")){
+            if (sc.matches("\\d+\\s+\\b[A-Za-zА-яёЁ]+")) {
+
                 String[] com = sc.split("\\s+", 2);
                 name = com[1];
                 number = com[0];
             }
             phoneBook.put(name, number);
 
-            if (phoneBook.containsKey(name)) {
-                printMap(name, number);
-                continue;
-            }
-            if (phoneBook.containsKey(number)) {
-                printMap();
-                continue;
-            }
+
         }
     }
-    private static void printMap(Map<String, String>map){
-        for (String key :map.keySet()){
-            System.out.println(key + "< = >" + map.get(key));
+
+    private static void printMap(Map<String, String> map) {
+        for (String key : map.keySet()) {
+            System.out.println(key + " < = > " + map.get(key));
         }
     }
 }
