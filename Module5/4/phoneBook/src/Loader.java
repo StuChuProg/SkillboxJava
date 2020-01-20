9e0bd723a0d987b39f181de523ba82716a21f745
ana <anas-chuamkova@yandex.ru>
2020-01-20 22:21:51 +0300

    add work code phoneBook change

--- Module5/4/phoneBook/src/Loader.java
+++ Module5/4/phoneBook/src/Loader.java
@@ -1,58 +1,80 @@
 import java.util.*;
 
 public class Loader {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Пожалуйста, введите телефон и/или имя");
+
         TreeMap<String, String> phoneBook = new TreeMap<>();
         Scanner scanner = new Scanner(System.in);
-        String name = " ";
-       String number = " ";
-        for (;;){
 
+        for (; ; ) {
             String sc = scanner.nextLine().trim();
-            if(sc.equals("LIST")){
+            if (sc.matches("LIST")) {
                 printMap(phoneBook);
                 continue;
             }
+            if (phoneBook.containsKey(sc) || phoneBook.containsValue(sc)) {
+                Set<Map.Entry<String, String>> set;
+                set = phoneBook.entrySet();
+                for (Map.Entry<String, String> me : set) {
+                    System.out.print(me.getKey() + " < = > " + me.getValue());
+                }
+            } else {
+                if (sc.matches("\\b[A-Za-zА-яёЁ]+")) {
+                    String name = sc;
+                    System.out.println("Введите номер телефона");
+                    String number = scanner.nextLine().trim();
+                    if (phoneBook.containsValue(number)) {
+                        System.out.println("Данный номеру присвоено другмое имя");
+                    } else {
+                        phoneBook.put(name, number);
+                    }
+                }
+                if (sc.matches("\\d+")) {
+                    String number = sc;
+                    System.out.println("Введите Имя");
+                    String name = scanner.nextLine().trim();
+                    if (phoneBook.containsValue(name)) {
+                        System.out.println("Данное имя присвоено другому номеру");
+                    } else {
+                        phoneBook.put(name, number);
+                    }
+                }
 
-          //  int key = 0;
-        //    int count = 0;
-           // for (int i = 0; i < phoneBook.size(); i++) {
-             //   if (phoneBook.containsKey(name)) {
-               //     count++;
-            //        key = i;
-              //      continue;}
-          //      if (phoneBook.containsValue(number)) {
-            //        count++;
-              //      key = i;;
-                //    continue; }}
-        //    if (count >= 2){
-        //        System.out.println(phoneBook.get(name) + " < = > " + phoneBook.get(phoneBook.get(key))); }
+                if (sc.matches("\\b[A-Za-zА-яёЁ]+\\s+\\d+")) {
 
-            if (sc.matches("\\d+")){
-                name = sc;
-                System.out.println("Введите имя абонента");
+                    String[] com = sc.split("\\s+", 2);
+                    String name = com[0].trim();
+                    String number = com[1].trim();
+                    if (phoneBook.containsValue(name)) {
+                        System.out.println("Данное имя присвоено другому номеру");
+                        continue;
+                    }
+                    if (phoneBook.containsValue(number)) {
+                        System.out.println("Данный номеру присвоено другмое имя");
+                        continue;
+                    } else {
 
+                        phoneBook.put(name, number);
+                    }
+                }
+                if (sc.matches("\\d+\\s+\\b[A-Za-zА-яёЁ]+")) {
+
+                    String[] com = sc.split("\\s+", 2);
+                    String name = com[1].trim();
+                    String number = com[0].trim();
+                    if (phoneBook.containsValue(name)) {
+                        System.out.println("Данное имя присвоено другому номеру");
+                        continue;
+                    }
+                    if (phoneBook.containsValue(number)) {
+                        System.out.println("Данный номеру присвоено другмое имя");
+                        continue;
+                    } else {
+                        phoneBook.put(name, number);
+                    }
+                }
             }
-            if (sc.matches("\\b[A-Za-zА-яёЁ]+")) {
-                System.out.println("Введите телефон");
-                number = sc;
-            }
-            if (sc.matches("\\b[A-Za-zА-яёЁ]+\\s+\\d+")){
-
-                String[] com = sc.split("\\s+", 2);
-                name = com[0];
-                number = com[1];
-            }
-            if (sc.matches("\\d+\\s+\\b[A-Za-zА-яёЁ]+")) {
-
-                String[] com = sc.split("\\s+", 2);
-                name = com[1];
-                number = com[0];
-            }
-            phoneBook.put(name, number);
-
-
         }
     }
 
