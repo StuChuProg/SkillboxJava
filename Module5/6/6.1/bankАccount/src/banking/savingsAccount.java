862b15ec7c924b732dc83686a67860fdc76d3c76
ana <anas-chuamkova@yandex.ru>
2020-01-31 12:54:57 +0300

    add code bankAccount work

--- Module5/6/6.1/bankАccount/src/banking/savingsAccount.java
+++ Module5/6/6.1/bankАccount/src/banking/savingsAccount.java
@@ -6,42 +6,36 @@ import java.util.Date;
 import java.util.GregorianCalendar;
 
 public class savingsAccount extends bankAccount{
+
     @Override
     public int getAccountStatusNOW() {
-        return super.getAccountStatusNOW();
-    }
-
+        return super.getAccountStatusNOW();    }
     @Override
     public void setAccountStatusNOW(int accountStatusNOW) {
         super.setAccountStatusNOW(accountStatusNOW);
     }
- 
-
-Calendar calendar = Calendar.getInstance();
+   Calendar calendar = Calendar.getInstance();
     Date dateDep;
-
-    public Date getNextDate() {
-        calendar.setTime(dateDep);
+    public int getDeposit(){
+        dateDep = new Date();
+        return accountStatusNOW + cashDeposit;
+    }
+    public Date getNextDate(Date date){
+        calendar.setTime(date);
         calendar.add(Calendar.MONTH, 1);
         Date dateNext = calendar.getTime();
         return dateNext;
     }
-
-    public int getDeposit() {
-        return accountStatusNOW + cashDeposit;
-//        dateDep = calendar.getTime();
-        dateDep = new Date();
-    }
-
-    Date dateNow = new Date();
-
-    if (dateNow.after(getNextDate(dateDep)) == true)    {
-        public int getWithdrawal() {
-            return accountStatusNOW - cashWithdrawal;
+    public int getWithdrawal(){
+        Date dateNow = new Date();
+        int x = 0;
+        if(dateNow.after(getNextDate(dateDep))){
+            x = accountStatusNOW - cashWithdrawal;
+        }else {
+            System.out.println("Срок выдачи денег не пришел!");
         }
+        return x;
     }
-    else
-    {
-        System.out.println("Срок выдачи денег не пришел! Вы не можете снять средства!");
-    }
+
+
 }
